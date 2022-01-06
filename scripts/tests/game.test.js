5b99c2ffdf3c90df1d3b7d2f86dbb2bbe49c2784
BrianStritch <brians12ca18@gmail.com>
2022-01-06 00:22:21 +0000

    add currentGame reset, playerMoves reset and display score function to game with tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,9 @@
  * @jest-environment jsdom
  */
 
-const {game} = require('../game');
+
+
+const {game, newGame, showScore} = require('../game');
 
 
 beforeAll(() => {  // typical boilerplate document loading code block
@@ -28,5 +30,27 @@ describe('game object contains the correct keys', () => {
     });
     test('choices contains the correct ids', () => {
         expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4']);
+    });
+});
+
+describe('newGame works correctly', () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ['1','2'];
+        game.currentGame = ['1','2'];
+        document.getElementById('score').innerText = '42';
+        newGame();
+    });
+    test('should set the game score to zero', ()=> {
+        expect(game.score).toEqual(0);
+    });
+    test('should set the playerMoves to an empty array', ()=> {
+        expect(game.playerMoves.length).toEqual(0);
+    });
+    test('should set the currentGame to an empty array', ()=> {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    test('should display zero in the element with an id of score', ()=> {
+        expect(document.getElementById('score').innerText).toEqual(0);
     });
 });
